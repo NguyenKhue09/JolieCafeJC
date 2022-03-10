13a54a91d3b29556713650a3b5de3c35cb11804e
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-03-11 02:12:11 +0700

    init project

---
+++ app/src/main/java/com/khue/joliecafejp/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.khue.joliecafejp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.khue.joliecafejp.ui.theme.JolieCafeJPTheme
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JolieCafeJPTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JolieCafeJPTheme {
+        Greeting("Android")
+    }
+}
