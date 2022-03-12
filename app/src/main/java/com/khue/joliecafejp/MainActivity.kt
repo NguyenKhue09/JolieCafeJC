0f6828cc7d6f2be75490a2011e0a83b82a9bccc8
NguyenKhue09 <19521712@gm.uit.edu.vn>
2022-03-12 13:08:41 +0700

    cÃonfig color and thÃeme

--- app/src/main/java/com/khue/joliecafejp/MainActivity.kt
+++ app/src/main/java/com/khue/joliecafejp/MainActivity.kt
@@ -4,25 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 import com.khue.joliecafejp.ui.theme.JolieCafeJPTheme
 import com.facebook.FacebookSdk
 import com.facebook.appevents.AppEventsLogger
+import com.khue.joliecafejp.ui.theme.grey_primary
+import com.khue.joliecafejp.ui.theme.montserratFontFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             JolieCafeJPTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = grey_primary
                 ) {
                     Greeting("Android")
                 }
@@ -33,7 +38,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        fontFamily = montserratFontFamily,
+        color = Color.White,
+        fontSize = 20.sp
+    )
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/khue/joliecafejp/ui/theme/Typography.kt
@@ -0,0 +1,14 @@
+package com.khue.joliecafejp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.khue.joliecafejp.R
+
+val festiveRegular = Font(R.font.festive_regular)
+val montserratRegular = Font(R.font.montserrat_regular)
+val ralewayRegular = Font(R.font.raleway_regular)
+val ralewayItalicVariableFontWght = Font(R.font.raleway_italic_variable_font_wght)
+val ralewayMedium = Font(R.font.raleway_medium)
+val ralewayVariableFontWght = Font(R.font.raleway_variable_font_wght)
+
+val montserratFontFamily = FontFamily(montserratRegular)
